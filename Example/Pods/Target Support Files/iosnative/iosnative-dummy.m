90a677c8c8a7136325a9d41be39b6bb83df8ae1b
darkmat13r <darkmat13r@gmail.com>
2023-12-06 16:50:16 +0530

    Initial commit

---
+++ Example/Pods/Target Support Files/iosnative/iosnative-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iosnative : NSObject
+@end
+@implementation PodsDummy_iosnative
+@end
---
+++ Example/iosnative/AppDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  AppDelegate.swift
+//  iosnative
+//
+//  Created by darkmat13r on 11/29/2023.
+//  Copyright (c) 2023 darkmat13r. All rights reserved.
+//
+
+import UIKit
+
